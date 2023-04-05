e8c61b259f52b4374d26c1e8584523b54e6de604
Sam Radford <smr1292@yahoo.com>
2023-04-05 19:38:33 -0400

    Brought in a warning square for player bomb hit points, made a flashing animation for it, started setting up process for detecting where a bomb will hit to spawn the warnings, which will transition into the explosions

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -5,9 +5,36 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     [SerializeField] int reach = 3;
+    [SerializeField] GameObject warningPrefab;
+
+    List<Vector2> validHitPoints;
+    List<GameObject> warnings;
+
+    private void Start() {
+        warnings = new List<GameObject>();
+        validHitPoints = GetValidHitPoints();
+        SpawnWarnings(validHitPoints);
+        validHitPoints.Insert(0, new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y)));
+    }
+
+    private List<Vector2> GetValidHitPoints() {
+        List<Vector2> result = new List<Vector2>();
+        Vector2 centeredPoint = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+        // Get each valid point and add to result
+        return result;
+    }
+
+    private void SpawnWarnings(List<Vector2> hitPoints) {
+        foreach(Vector2 hitPoint in hitPoints) {
+            warnings.Add(Instantiate(warningPrefab, hitPoint, Quaternion.identity));
+        }
+    }
 
     public void ExplodeBomb() {
         print("I exploded!");
+        foreach(GameObject warning in warnings) {
+            Destroy(warning);
+        }
         Destroy(gameObject);
     }
 
