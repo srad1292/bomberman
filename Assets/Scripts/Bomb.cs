ee3089748b9187ea7af648ca7588048c74addfcd
Sam Radford <smr1292@yahoo.com>
2023-04-06 16:54:54 -0400

    Added explosion script that takes in sprites and time between switches to animate sequence of sprites.  Bomb scripts updated to determine valid locations/directions that can be hit. Can see huge bomb shows warnings, then explodes with animation correctly.

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -6,36 +6,93 @@ public class Bomb : MonoBehaviour
 {
     [SerializeField] int reach = 3;
     [SerializeField] GameObject warningPrefab;
+    [SerializeField] GameObject originExplosionPrefab;
+    [SerializeField] GameObject directionalExplosionPrefab;
+    [SerializeField] LayerMask blockingMask;
+    [SerializeField] LayerMask destructibleMask;
 
-    List<Vector2> validHitPoints;
+    List<Vector2> validHitPointsAndDirections;
     List<GameObject> warnings;
+
+    Quaternion upRotation = new Quaternion(0, 0, 0.707106829f, -0.707106829f);
+    Quaternion rightRotation = new Quaternion(0, 0, 1, 0);
+    Quaternion downRotation = new Quaternion(0, 0, 0.707106829f, 0.707106829f);
+    Quaternion leftRotation = new Quaternion(0, 0, 0, 1);
 
     private void Start() {
         warnings = new List<GameObject>();
-        validHitPoints = GetValidHitPoints();
-        SpawnWarnings(validHitPoints);
-        validHitPoints.Insert(0, new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y)));
+        validHitPointsAndDirections = GetValidHitPoints();
+        SpawnWarnings(validHitPointsAndDirections);
     }
 
     private List<Vector2> GetValidHitPoints() {
         List<Vector2> result = new List<Vector2>();
-        Vector2 centeredPoint = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+        Vector2 centerPoint = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
         // Get each valid point and add to result
+        Vector2[] directions = { Vector2.up, Vector2.right, Vector2.down, Vector2.left };
+        foreach(Vector2 direction in directions) {
+            Vector2 currentPoint = centerPoint;
+            int distance = 0;
+            bool blocked = false;
+            while(distance < reach && !blocked) {
+                // raycast in direction
+                RaycastHit2D blockHit = Physics2D.Raycast(currentPoint, direction, 1f, blockingMask);
+                RaycastHit2D destructibleHit = Physics2D.Raycast(currentPoint, direction, 1f, destructibleMask);
+                if (blockHit.collider != null) {
+                    blocked = true;
+                }
+                else if (destructibleHit.collider != null) {
+                    blocked = true;
+                }
+                else {
+                    currentPoint += direction;
+                    result.Add(currentPoint);
+                    result.Add(direction);
+                }
+                distance++;
+            }
+        }
         return result;
     }
 
     private void SpawnWarnings(List<Vector2> hitPoints) {
-        foreach(Vector2 hitPoint in hitPoints) {
-            warnings.Add(Instantiate(warningPrefab, hitPoint, Quaternion.identity));
+        for(int idx = 0; idx < hitPoints.Count; idx+=2) { 
+            warnings.Add(Instantiate(warningPrefab, hitPoints[idx], Quaternion.identity));
         }
     }
 
     public void ExplodeBomb() {
-        print("I exploded!");
         foreach(GameObject warning in warnings) {
             Destroy(warning);
         }
+        SpawnExplosions(validHitPointsAndDirections);
         Destroy(gameObject);
+    }
+
+    private void SpawnExplosions(List<Vector2> hitPoints) {
+        Vector2 centerPoint = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+        gameObject.SetActive(false);
+        Instantiate(originExplosionPrefab, centerPoint, Quaternion.identity);
+        Vector2 hitPosition, hitDirection;
+        Quaternion hitRotation;
+        for (int idx = 0; idx < hitPoints.Count; idx+=2) {
+            hitPosition = hitPoints[idx];
+            if(idx+1 < hitPoints.Count) {
+                hitDirection = hitPoints[idx+1];
+                if(hitDirection == Vector2.up) {
+                    hitRotation = upRotation;
+                } else if (hitDirection == Vector2.right) {
+                    hitRotation = rightRotation;
+                } else if (hitDirection == Vector2.down) {
+                    hitRotation = downRotation;
+                } else {
+                    hitRotation = leftRotation;
+                }
+                Instantiate(directionalExplosionPrefab, hitPosition, hitRotation);
+
+            }
+        }
+
     }
 
 }
--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] Sprite[] sprites;
+    [SerializeField] float spriteDuration = 0.2f;
+    [SerializeField] bool doReverse = true;
+
+    SpriteRenderer mySpriteRenderer;
+
+    int spriteIndex = 0;
+    float counter = 0f;
+    bool goingInReverse = false;
+
+
+    private void Start() {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(sprites.Length == 0 || mySpriteRenderer == null) {
+            print("Explosion: Missing sprites or renderer");
+            Destroy(gameObject);
+        }
+        counter += Time.deltaTime;
+        if(counter >= spriteDuration) {
+            if (goingInReverse) {
+                if(spriteIndex - 1 > 0) {
+                    spriteIndex--;
+                    mySpriteRenderer.sprite = sprites[spriteIndex];
+                    counter = 0f;
+                } else {
+                    Destroy(gameObject);
+                }
+            }
+            else if(doReverse) {
+                if(spriteIndex+1 >= sprites.Length ) {
+                    goingInReverse = true;
+                    counter = 0f;
+                } else {
+                    spriteIndex++;
+                    mySpriteRenderer.sprite = sprites[spriteIndex];
+                    counter = 0f;
+                }
+            } else {
+                spriteIndex++;
+                if(spriteIndex >= sprites.Length) {
+                    Destroy(gameObject);
+                } else {
+                    mySpriteRenderer.sprite = sprites[spriteIndex];
+                    counter = 0f;
+                }
+            }
+        }
     }
 }
