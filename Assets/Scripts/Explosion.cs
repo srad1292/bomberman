0b2082ace405810f15fa24440b5368c73fdc1f76
Sam Radford <smr1292@yahoo.com>
2023-04-07 17:24:51 -0400

    Imported assets for basic/big bomb, normal explosion, and crate breaking. Moved the sprite flipbook scripted animation code from explosion to its own SpriteFlipbook script. Updated explosion prefabs to use new script

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -4,56 +4,5 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-    [SerializeField] Sprite[] sprites;
-    [SerializeField] float spriteDuration = 0.2f;
-    [SerializeField] bool doReverse = true;
-
-    SpriteRenderer mySpriteRenderer;
-
-    int spriteIndex = 0;
-    float counter = 0f;
-    bool goingInReverse = false;
-
-
-    private void Start() {
-        mySpriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    void Update()
-    {
-        if(sprites.Length == 0 || mySpriteRenderer == null) {
-            print("Explosion: Missing sprites or renderer");
-            Destroy(gameObject);
-        }
-        counter += Time.deltaTime;
-        if(counter >= spriteDuration) {
-            if (goingInReverse) {
-                if(spriteIndex - 1 > 0) {
-                    spriteIndex--;
-                    mySpriteRenderer.sprite = sprites[spriteIndex];
-                    counter = 0f;
-                } else {
-                    Destroy(gameObject);
-                }
-            }
-            else if(doReverse) {
-                if(spriteIndex+1 >= sprites.Length ) {
-                    goingInReverse = true;
-                    counter = 0f;
-                } else {
-                    spriteIndex++;
-                    mySpriteRenderer.sprite = sprites[spriteIndex];
-                    counter = 0f;
-                }
-            } else {
-                spriteIndex++;
-                if(spriteIndex >= sprites.Length) {
-                    Destroy(gameObject);
-                } else {
-                    mySpriteRenderer.sprite = sprites[spriteIndex];
-                    counter = 0f;
-                }
-            }
-        }
-    }
+    
 }
---
+++ Assets/Scripts/SpriteFlipbook.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFlipbook : MonoBehaviour
+{
+    [SerializeField] Sprite[] sprites;
+    [SerializeField] float spriteDuration = 0.2f;
+    [SerializeField] bool doReverse = true;
+
+    SpriteRenderer mySpriteRenderer;
+
+    int spriteIndex = 0;
+    float counter = 0f;
+    bool goingInReverse = false;
+
+
+    private void Start() {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        if (sprites.Length == 0 || mySpriteRenderer == null) {
+            print("Flipbook: Missing sprites or renderer");
+            Destroy(gameObject);
+        }
+        counter += Time.deltaTime;
+        if (counter >= spriteDuration) {
+            if (goingInReverse) {
+                if (spriteIndex - 1 > 0) {
+                    spriteIndex--;
+                    mySpriteRenderer.sprite = sprites[spriteIndex];
+                    counter = 0f;
+                }
+                else {
+                    Destroy(gameObject);
+                }
+            }
+            else if (doReverse) {
+                if (spriteIndex + 1 >= sprites.Length) {
+                    goingInReverse = true;
+                    counter = 0f;
+                }
+                else {
+                    spriteIndex++;
+                    mySpriteRenderer.sprite = sprites[spriteIndex];
+                    counter = 0f;
+                }
+            }
+            else {
+                spriteIndex++;
+                if (spriteIndex >= sprites.Length) {
+                    Destroy(gameObject);
+                }
+                else {
+                    mySpriteRenderer.sprite = sprites[spriteIndex];
+                    counter = 0f;
+                }
+            }
+        }
+    }
+}
