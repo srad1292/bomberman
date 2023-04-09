545fab8cc52f4dac557f9d3071ec1d1e3d0d5272
Sam Radford <smr1292@yahoo.com>
2023-04-09 10:24:14 -0400

    Spawning bombs on player input

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,39 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] GameObject bombPrefab;
+    [SerializeField] float reloadTime = 0.8f;
+
+    private bool canSpawnBomb = true;
+
     private void OnTriggerEnter2D(Collider2D otherObject) {
         if (otherObject.gameObject.tag == "Stairs") {
             LevelManager.Instance.LevelCompleted();
         }
     }
+
+    void OnFire(InputValue value) {
+        SpawnBomb();
+    }
+
+    private void SpawnBomb() {
+        if(!canSpawnBomb) { return; }
+
+        canSpawnBomb = false;
+        Vector2 centerPoint = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+        Instantiate(bombPrefab, centerPoint, Quaternion.identity);
+        StartCoroutine(Reload());
+    }
+
+    IEnumerator Reload() {
+        yield return new WaitForSeconds(reloadTime);
+        canSpawnBomb = true;
+    }
+
+
+    
 }
