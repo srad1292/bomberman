ebb29797a4b9ce6872ddcb9d153da369d7dfcfd6
Sam Radford <smr1292@yahoo.com>
2023-04-14 17:01:43 -0400

    In process of making main menu and credits scene

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
@@ -16,5 +17,14 @@ public class LevelManager : MonoBehaviour
 
     public void LevelCompleted() {
         print("You completed the level!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadMainMenu() {
+        SceneManager.LoadScene("MainMenu");
     }
 }
